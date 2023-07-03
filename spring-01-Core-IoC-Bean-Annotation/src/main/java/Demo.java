63a87b911ef5ae6883011802687810523fa2b9e2
MertRam35 <mert.ramadan35@gmail.com>
2023-07-03 09:17:49 +0300

    first class added

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/Demo.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/Demo.java
@@ -1,8 +1,11 @@
+import com.github.javafaker.Faker;
+
 public class Demo {
 
     public static void main(String[] args) {
 
-
+        Faker faker = new Faker();
+        System.out.println("faker.harryPotter().house() = " + faker.harryPotter().house());
 
 
     }
